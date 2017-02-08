15abc12c0f2306864bee07f4ef1463d4784833a5
Richard Lee <richard.lee@sabin.io>
2017-02-08 11:58:24 +0000

    Initial Commit of Canard
    Canard aims to provide demos to workaround issues in SSDT. And to quash the unfounded rumours that SSDT is rubbish.

---
+++ Canard/Canard/dbo/Tables/ConstraintSyntax.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[ConstraintSyntax] (
+    [OrderID]        INT           NOT NULL,
+    [CustomerID]     NCHAR (5)     NULL,
+    [EmployeeID]     INT           NULL,
+    [OrderDate]      DATETIME      NULL,
+    [RequiredDate]   DATETIME      NULL,
+    [ShippedDate]    DATETIME      NULL,
+    [Freight]        MONEY         NULL,
+    [ShipName]       NVARCHAR (40) NULL,
+    [ShipAddress]    NVARCHAR (60) NULL,
+    [ShipCity]       NVARCHAR (15) NULL,
+    [ShipRegion]     NVARCHAR (15) NULL,
+    [ShipPostalCode] NVARCHAR (10) NULL,
+    [ShipCountry]    NVARCHAR (15) NULL,
+    [ShippedBy]      NVARCHAR (64) NULL
+	,
+    CONSTRAINT [CK_Order_ShippedBy] CHECK (
+	[ShippedBy]IN (
+'Speedy  Express'
+,'Speedy  Package'
+,'Speedy  Shipping'
+,'United  Express'
+,'United  Package'
+,'United  Shipping'
+,'Federal  Express'
+,'Federal  Package'
+,'Federal  Shipping'
+	))
+);
+
---
+++ Canard/Canard/dbo/Tables/Orders.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderID]        INT           NOT NULL,
+    [CustomerID]     NCHAR (5)     NULL,
+    [EmployeeID]     INT           NULL,
+    [OrderDate]      DATETIME      NULL,
+    [RequiredDate]   DATETIME      NULL,
+    [ShippedDate]    DATETIME      NULL,
+    [Freight]        MONEY         NULL,
+    [ShipName]       NVARCHAR (40) NULL,
+    [ShipAddress]    NVARCHAR (60) NULL,
+    [ShipCity]       NVARCHAR (15) NULL,
+    [ShipRegion]     NVARCHAR (15) NULL,
+    [ShipPostalCode] NVARCHAR (10) NULL,
+    [ShipCountry]    NVARCHAR (15) NULL,
+    [ShippedBy]      NVARCHAR (64) NULL
+);
+
+
+GO
+CREATE UNIQUE CLUSTERED INDEX [CI_OrderID]
+    ON [dbo].[Orders]([OrderID] ASC);
+
